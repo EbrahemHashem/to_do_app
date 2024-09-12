03c16cb5ec4eefbfcfe29db5eaee1205d1d59adc
Ebrahem <ebrahemhashem72@gmail.com>
2024-09-12 21:07:36 +0300

    create type adabter for to do model (hive)

--- lib/models/to_do_model.dart
+++ lib/models/to_do_model.dart
@@ -1,6 +1,13 @@
-class ToDoModel {
+import 'package:hive/hive.dart';
+part 'to_do_model.g.dart';
+
+@HiveType(typeId: 0)
+class ToDoModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final bool value;
+  @HiveField(2)
   void Function(bool?) onChanged;
 
   ToDoModel({
