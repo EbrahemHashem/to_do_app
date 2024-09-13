169d5d0ff979a29afe2ac54aed1229feacfbf3d1
Ebrahem <ebrahemhashem72@gmail.com>
2024-09-13 16:33:29 +0300

    delete todo

--- lib/widgets/custom_to_do_list.dart
+++ lib/widgets/custom_to_do_list.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:to_do_app/models/to_do_model.dart';
 
 class CustomToDoList extends StatefulWidget {
@@ -14,34 +15,54 @@ class CustomToDoList extends StatefulWidget {
 class _CustomToDoListState extends State<CustomToDoList> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(18),
-      child: Container(
-        padding: const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Colors.yellow,
-        ),
-        child: Row(children: [
-          // check box
-          Checkbox(
-            value: widget.toDoModel.isDone,
-            activeColor: Colors.black87,
-            onChanged: (bool? value) {
-              widget.toDoModel.isDone = value!;
-              setState(() {});
+    return Slidable(
+      // slidable
+
+      key: const ValueKey(ToDoModel),
+      closeOnScroll: true,
+      endActionPane: ActionPane(
+        dismissible: DismissiblePane(onDismissed: () {}),
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            backgroundColor: Colors.red,
+            borderRadius: BorderRadius.circular(12),
+            onPressed: (context) {
+              widget.toDoModel.delete();
             },
-          ),
-          // title
-          Text(
-            widget.toDoModel.title,
-            style: TextStyle(
-              fontSize: 18,
-              decoration: widget.toDoModel.isDone ? TextDecoration.lineThrough : TextDecoration.none,
-            ),
-          ),
-        ]),
+            icon: Icons.delete,
+          )
+        ],
       ),
+      // end slidable
+      child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: Container(
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.yellow,
+            ),
+            child: Row(children: [
+              // check box
+              Checkbox(
+                value: widget.toDoModel.isDone,
+                activeColor: Colors.black87,
+                onChanged: (bool? value) {
+                  widget.toDoModel.isDone = value!;
+                  setState(() {});
+                },
+              ),
+              // title
+              Text(
+                widget.toDoModel.title,
+                style: TextStyle(
+                  fontSize: 18,
+                  decoration: widget.toDoModel.isDone ? TextDecoration.lineThrough : TextDecoration.none,
+                ),
+              ),
+            ]),
+          )),
     );
   }
 }
